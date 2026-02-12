ba90ca066df6102a208156c9c23bd49d773457cc
asiel <br630455@gmail.com>
2026-02-11 21:20:06 -0700

    Upd - comentarios

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Esto establece la conexion con la bd
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/toroeats"
+#para que la conexion sea exitosa, es recomendable 
+# que el usuario de pgadmin sea postgres y su contrasenia igual
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
