6c74c79986e9cec2154c9616df8bcf407c435a3f
asiel <br630455@gmail.com>
2026-04-10 08:45:31 -0600

    Actualizacion a pre-despliegue

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import  os
 
 #:> Este archivo centraliza la conexion a la BD para que todo el backend use la misma base
 #:> engine abre la conexion general con postgres
 #:> SessionLocal crea sesiones cortas para cada request
 #:> Base sirve como clase madre para todos los modelos
 # Esto establece la conexion con la bd
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/toroeats"
+DATABASE_URL = os.getenv("DATABASE_URL")
 #para que la conexion sea exitosa, es recomendable 
 # que el usuario de pgadmin sea postgres y su contrasenia igual
 
